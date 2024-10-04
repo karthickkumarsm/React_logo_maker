7d6544f581972208ddcc278246b4f5caee471951
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-05 00:01:15 +0530

    added shadcn ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#F15757',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
