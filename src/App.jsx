18d81d65ab81d6f7bc049a2eb06945bef7b75d3d
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-05 22:21:01 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from "@/components/ui/button"
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button>Click me</Button>
+      <Header/>
     </>
   )
 }
