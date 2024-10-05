6f4cb9c642f54b9ba6a6d508f73428a2b80e1738
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-05 23:14:16 +0530

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import SideNav from './components/SideNav'
 
 function App() {
   return (
     <>
       <Header/>
+      <div className="w-64 fixed">
+      <SideNav selectedIndex={(value)=>console.log(value)}/>
+      </div>
+      <div className="ml-64">
+        Body
+      </div>
     </>
   )
 }
