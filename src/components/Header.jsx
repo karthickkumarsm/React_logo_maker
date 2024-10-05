98dca415fa3108e5e7a14edef04785e425f0ce49
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-05 22:35:08 +0530

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Button } from './ui/button'
+import { Download } from 'lucide-react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='p-4 shadow-sm border flex justify-between items-center'>
+        <img src='/src/assets/image.png' className='w-[170px]'/>
+        <Button className='flex gap-2 items-center'><Download className='h-4 w-4'/>Download</Button>
+    </div>
   )
 }
 
