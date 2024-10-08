911b796e944ae68247d5658b3ff552a0491ba7ea
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-08 22:41:35 +0530

    added sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Button } from './ui/button'
-import { Download } from 'lucide-react'
+import React from 'react';
+import { Button } from './ui/button';
+import { Download } from 'lucide-react';
 
 function Header() {
   return (
