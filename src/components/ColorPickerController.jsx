0199d986cfba61ae4d2c7c4ae6fc2840230216e9
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-09 11:37:58 +0530

    added icon controller functionality

---
+++ src/components/ColorPickerController.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ColorPicker from 'react-best-gradient-color-picker'
+import { useState } from 'react'
+
+function ColorPickerController({hideController=false,selectedColor}) {
+    const [color, setColor] = useState('rgba(255,255,255,1)');
+
+  return (
+    <div>
+        <ColorPicker value={color} onChange={(e)=>{setColor(e);selectedColor(e)}} hideControls={hideController} hideEyeDrop hideAdvancedSliders hideColorGuide hideInputType/>
+    </div>
+  )
+}
+
+export default ColorPickerController
--- src/components/IconController.jsx
+++ src/components/IconController.jsx
@@ -2,11 +2,27 @@ import { Smile } from 'lucide-react'
 import React from 'react'
 import { Slider } from "@/components/ui/slider"
 import { useState } from 'react'
+import ColorPickerController from './ColorPickerController';
 
 
 function IconController() {
     const [size, setSize] = useState(280);
     const [rotate, setRotate] = useState(0);
+    const [color, setColor] = useState('rgba(255,255,255,1)');
+    const storageValue = JSON.parse(localStorage.getItem('value'));
+
+    useEffect(() => {
+      const updatedValue = {
+        ...storageValue,
+        iconSize: size,
+        iconRotate: rotate,
+        iconColor: color,
+        icon:'Smile'
+      }
+      localStorage.setItem('value', JSON.stringify(updatedValue));
+    }, [size,rotate,color])
+    
+
   return (
     <div>
         <div>
@@ -21,6 +37,10 @@ function IconController() {
             <div className="py-2">
                 <label className='p-2 flex justify-between items-center'>Rotate <span>{rotate}°</span></label>
                 <Slider defaultValue={[0]} max={360} step={1} onValueChange={(event)=>setRotate(event[0])} />
+            </div>
+            <div className="py-2">
+                <label className='p-2 flex justify-between items-center'>Icon Color</label>
+                <ColorPickerController hideController={true} selectedColor={(color) => setColor(color)}/>
             </div>
         </div>
     </div>
