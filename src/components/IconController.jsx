e4a4c125f6112dfe69768070379be5411eca1431
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-14 23:45:43 +0530

    added logo preview UI and some funtionality

--- src/components/IconController.jsx
+++ src/components/IconController.jsx
@@ -8,8 +8,8 @@ import { UpdateStorageContext } from '../context/UpdateStorageContext';
 
 
 function IconController() {
-    const [size, setSize] = useState(280);
-    const [rotate, setRotate] = useState(0);
+    const [size, setSize] = useState(storageValue?storageValue?.iconSize:280);
+    const [rotate, setRotate] = useState(storageValue?storageValue?.iconRotate:0);
     const [color, setColor] = useState('rgba(255,255,255,1)');
     const storageValue = JSON.parse(localStorage.getItem('value'));
     const {updateStorage,setUpdateStorage} = useContext(UpdateStorageContext);
@@ -36,11 +36,11 @@ function IconController() {
             </div>
             <div className="py-2">
                 <label className='p-2 flex justify-between items-center'>Size <span>{size}px</span></label>
-                <Slider defaultValue={[280]} max={512} step={1} onValueChange={(event)=>setSize(event[0])} />
+                <Slider defaultValue={[size]} max={512} step={1} onValueChange={(event)=>setSize(event[0])} />
             </div>
             <div className="py-2">
                 <label className='p-2 flex justify-between items-center'>Rotate <span>{rotate}°</span></label>
-                <Slider defaultValue={[0]} max={360} step={1} onValueChange={(event)=>setRotate(event[0])} />
+                <Slider defaultValue={[rotate]} max={360} step={1} onValueChange={(event)=>setRotate(event[0])} />
             </div>
             <div className="py-2">
                 <label className='p-2 flex justify-between items-center'>Icon Color</label>
--- src/components/LogoPreview.jsx
+++ src/components/LogoPreview.jsx
@@ -1,5 +1,6 @@
 import {useEffect,useState,useContext} from 'react'
 import { UpdateStorageContext } from '../context/UpdateStorageContext';
+import { icons } from 'lucide-react';
 
 function LogoPreview() {
 
@@ -10,15 +11,27 @@ function LogoPreview() {
         const storageData = JSON.parse(localStorage.getItem('value'));
         setStorageValue(storageData);
     }, [updateStorage])
+
+    const Icon = ({name,color,size}) => {
+        const LucidIcon = icons[name];
+        if(!LucidIcon) return;
+        return <LucidIcon size={size} color={color} style={{
+            transform:`rotate(${storageValue?.iconRotate}deg)`
+        }}/>
+    }
     
   return (
     <div className='flex items-center justify-center h-screen'>
-        <div className="h-[450px] w-[450px] bg-gray-200 outline-dotted outline-gray-300">
-            <div className="h-full w-full" style={{
+        <div className="h-[450px] w-[450px] bg-gray-200 outline-dotted outline-gray-300"
+        style={{
+            padding:storageValue?.bgPadding,
+        }}
+        >
+            <div className="h-full w-full flex items-center justify-center" style={{
                 borderRadius:storageValue?.bgRounded,
                 background:storageValue?.bgColor,
                 }}>
-
+                <Icon name={storageValue?.icon} color={storageValue?.iconColor} size={storageValue?.iconSize} />
             </div>
         </div>
     </div>
