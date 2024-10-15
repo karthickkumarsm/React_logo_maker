f5c7eab90425e323cb595187cbe8bc8332c45ee9
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-15 23:34:36 +0530

    added logo selection list UI

---
+++ src/components/IconList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  
+
+function IconList() {
+  return (
+    <div>IconList</div>
+  )
+}
+
+export default IconList
