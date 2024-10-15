1d025726bbcfbe4dd61e66127d4f4d56e7494804
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-16 00:15:15 +0530

    added logo selection list UI

--- src/components/IconList.jsx
+++ src/components/IconList.jsx
@@ -15,6 +15,7 @@ import { iconList } from '@/constants/icons';
 function IconList({selectedIcon}) {
     const [openDialog, setOpenDialog] = useState(false);
     const storageValue = JSON.parse(localStorage.getItem('value'));
+    const [icon, setIcon] = useState(storageValue?storageValue?.icon: 'Smile');
 
     const Icon = ({name,color,size}) => {
         const LucidIcon = icons[name];
@@ -27,7 +28,7 @@ function IconList({selectedIcon}) {
         <div className="">
             <label>Icon</label>
             <div onClick={()=>setOpenDialog(true)} className="p-3 cursor-pointer bg-gray-200 rounded-md w-[50px] h-[50px] flex items-center justify-center my-2">
-            <Smile/>
+            <Icon name={icon} size={20} color='black'/>
             </div>
         </div>
         <Dialog open={openDialog}>
@@ -37,7 +38,7 @@ function IconList({selectedIcon}) {
             <DialogDescription>
                 <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 overflow-auto h-[400px] p-6">
                     {iconList.map((icon,index)=>(
-                        <div className='border p-3 flex rounded-sm items-center justify-center cursor-pointer' onClick={()=>{selectedIcon(icon);setOpenDialog(false)}}>
+                        <div className='border p-3 flex rounded-sm items-center justify-center cursor-pointer' onClick={()=>{selectedIcon(icon);setOpenDialog(false);setIcon(icon)}}>
                             <Icon name={icon} size={20} color='black'/>
                         </div>
                     ))
