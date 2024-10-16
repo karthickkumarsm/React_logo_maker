a90521ebda27773db1eab2e80cb150e7848f051d
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-10-16 23:52:12 +0530

    added download icon functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Button } from './ui/button';
 import { Download } from 'lucide-react';
 
-function Header() {
+function Header({DownloadIcon}) {
   return (
     <div className='p-4 shadow-sm border flex justify-between items-center'>
         <img src='/src/assets/image.png' className='w-[170px]'/>
-        <Button className='flex gap-2 items-center'><Download className='h-4 w-4'/>Download</Button>
+        <Button className='flex gap-2 items-center' onClick={()=>DownloadIcon()}><Download className='h-4 w-4'/>Download</Button>
     </div>
   )
 }
